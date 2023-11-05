84446f74ee2f00c828d3569fe17bba9f67637639
Rafael Gutierrez <rgutierrez2021@fau.edu>
2023-11-05 16:03:40 -0500

    Initial Commit

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by rafael gutierrez on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
