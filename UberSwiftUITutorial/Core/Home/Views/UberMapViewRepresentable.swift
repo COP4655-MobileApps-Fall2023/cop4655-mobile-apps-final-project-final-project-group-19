d285b19b36b4cfc718cc0f6ec3b74e3fbb0b11b0
MD Jelani <mjelani2022@fau.edu>
2023-12-04 17:45:55 -0500

    Presentation logic for RIde Request View

--- UberSwiftUITutorial/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberSwiftUITutorial/Core/Home/Views/UberMapViewRepresentable.swift
@@ -87,7 +87,7 @@ extension UberMapViewRepresentable {
             polyline.strokeColor = .systemBlue
             polyline.lineWidth = 6
             return polyline
-            
+    
         }
         //helpers
         
@@ -99,13 +99,14 @@ extension UberMapViewRepresentable {
             parent.mapView.addAnnotation(anno)
             parent.mapView.selectAnnotation(anno, animated: true)
             
-            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
         }
         
         func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
             guard let userLocationCoordinate = self.userLocationCoordinate else {return}
              getDestinationRoute(from: userLocationCoordinate, to: coordinate) { route in
                 self.parent.mapView.addOverlay(route.polyline)
+                let rect = self.parent.mapView.mapRectThatFits(route.polyline.boundingMapRect, edgePadding: .init(top: 64, left: 32, bottom: 500, right: 32))
+                self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
             }
         }
         
